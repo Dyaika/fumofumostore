2ef531879ab5a0552783a93fc48ce67e13d9292d
Dyaika <91870391+Dyaika@users.noreply.github.com>
2022-11-30 20:34:32 +0300

    item почти рабочий, картинки меняет

--- script-change_product_attr.js
+++ script-change_product_attr.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
 
+    $('.version-choose label').on('click', function() {
+        var fumoVerr = $(this).attr('data-ver');
+        let image = document.getElementById('fumoImage');
+        let fumoName = document.getElementById('info-for-script').innerText;
+        image.src = "images/" + fumoName + fumoVerr + ".jpg";
+    });
+
+});
