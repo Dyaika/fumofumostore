5abcc699f3a1e3ce4998387b894e42df39ae94fd
Dyaika <91870391+Dyaika@users.noreply.github.com>
2022-12-03 15:16:46 +0300

    Отличный контент товаров генерируется по JSON. Добавление новых товаров не составит труда

--- script-change_product_attr.js
+++ script-change_product_attr.js
@@ -8,22 +8,70 @@ function getJsonData(){
         async: false});
     return $.parseJSON(jqXHR.responseText).fumos[fumoName];
 }
+//одинаковое для обеих версий
+let name = document.getElementById('name');
+let short_desc = document.getElementById('short_desc');
+let cite = document.getElementById('cite');
+let full_desc = document.getElementById('full_desc');
+let model_info = document.getElementById('model_info');
+let review_box = document.getElementById("reviews");
+
+//разное для двух версий
 let image = document.getElementById('fumoImage');
 let a = document.getElementById('buy');
 let year = document.getElementById('year');
 let cost = document.querySelector('.price');
 let firstLabel = document.querySelector('.vers-label1');
-let json_data = getJsonData();
+
 //предстартовый выбор
+let json_data = getJsonData();
 firstRadio.checked = true;
 let fumoVer = $(firstLabel).attr('data-ver');
 let cur_fumo = json_data[fumoVer];
 if (flag){
     flag = false;
+    //отличающиеся
     image.src = cur_fumo.image;
     a.href = cur_fumo.url;
     year.innerText = cur_fumo.year;
-    cost.innerHTML = cur_fumo.cost + "¥";
+    cost.innerText = cur_fumo.cost;
+
+    //отзывы
+    if (json_data.reviews.length === 0){
+        let no_revs_elem = document.createElement('div');
+        no_revs_elem.innerText = "Отзывов нет.";
+        no_revs_elem.style.color = 'white';
+        no_revs_elem.style.fontSize = '1.2rem';
+        no_revs_elem.style.textAlign = 'center';
+        let no_revs_link = document.createElement('a');
+        no_revs_link.href = "reviews.html";
+        no_revs_link.innerText = " Добавить!";
+        no_revs_link.style.textAlign = 'center';
+        review_box.appendChild(no_revs_elem);
+        no_revs_elem.appendChild(no_revs_link);
+    } else {
+        for (var i = 0; i < json_data.reviews.length; i++) {
+            let review = document.createElement('div');
+            review.className = "review";
+            let customer_name = document.createElement('div');
+            customer_name.className = "customer_name";
+            customer_name.innerText = json_data.reviews[i].rev_name;
+            let review_text = document.createElement('div');
+            review_text.className = "review_text";
+            review_text.innerText = json_data.reviews[i].rev_text;
+            review.appendChild(customer_name);
+            review.appendChild(review_text);
+            review_box.appendChild(review);
+        }
+    }
+    //одинаковые
+    name.innerText = json_data.name;
+    short_desc.innerText = json_data.short_desc;
+    cite.innerText = json_data.cite;
+    full_desc.innerText = json_data.full_desc;
+    model_info.innerText = json_data.model_info;
+
+
 }
 $('.version-choose label').on('click', function() {
     fumoVer = $(this).attr('data-ver');
@@ -32,5 +80,5 @@ $('.version-choose label').on('click', function() {
     image.src = cur_fumo.image;
     a.href = cur_fumo.url;
     year.innerText = cur_fumo.year;
-    cost.innerHTML = cur_fumo.cost + "¥";
+    cost.innerText = cur_fumo.cost;
 });
